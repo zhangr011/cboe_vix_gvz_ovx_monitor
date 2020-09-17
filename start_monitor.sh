cde3fad423495539bf50cd942352ace411ca900f
zhangr011 <zhangr011@gmail.com>
2020-09-17 10:58:32 +0800

    update start sh, for pid check

--- start_monitor.sh
+++ start_monitor.sh
@@ -1,2 +1,31 @@
-# bash
-nohup python3 ./main.py 2>&1 &
+#!/bin/bash
+
+SERVICE_NAME=cboe_monitor
+PID=$SERVICE_NAME.pid
+ProcNumber=`ps -ef | grep -w $SERVICE_NAME | grep -v grep | wc -l`
+
+case "$1" in
+    start)
+        if [ -f ./$PID ]; then
+            echo "$SERVICE_NAME is started, please use the restart option. "
+        else
+            nohup python3 ./cboe_monitor.py 2>&1 &
+            echo $! > ./$PID
+            echo "==== start $SERVICE_NAME ===="
+        fi
+        ;;
+    stop)
+        kill -9 `cat ./$PID`
+        rm -rf ./$PID
+        echo "==== stop $SERVICE_NAME ===="
+        ;;
+    restart)
+        $0 stop
+        sleep 2
+        $0 start
+        ;;
+    *)
+        echo "Usage: bash start_proxy.sh [start|stop|restart]"
+        ;;
+esac
+exit 0
